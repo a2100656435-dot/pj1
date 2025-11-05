c9c7764ca17785ac741efde388149667dc470fe9
a2100656435-dot <a2100656435@gmail.com>
2025-11-05 11:45:49 -0500

    runoob-test.sh

--- runoob-test.sh
+++ runoob-test.sh
@@ -1,8 +1,7 @@
-phishscan_app/
-│
-├── app.py                 # Flask 主程序
-├── templates/
-│   └── index.html         # 前端页面（上传界面）
-├── codes.json             # 存放合法身份码列表
-├── requirements.txt       # 依赖列表
-└── scanner.py             # 扫描逻辑（简化版）
+project/
+ ├─ app.py
+ ├─ requirements.txt
+ └─ templates/
+     └─ index.html
+ └─ uploads/        # 上传临时文件
+ └─ pdfs/           # 生成 PDF
