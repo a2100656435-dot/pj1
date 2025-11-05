b7af7232e30b0d1574e40ad37e1cc048f7a88da2
a2100656435-dot <a2100656435@gmail.com>
2025-11-05 12:15:36 -0500

    Update runoob-test.sh

--- runoob-test.sh
+++ runoob-test.sh
@@ -2,7 +2,8 @@ project/
  ├─ app.py                  # 主 Flask 应用
  ├─ requirements.txt
  ├─ uploads/                # 临时上传文件（自动清理）
- ├─ pdfs/                   # 生成的 PDF 文件
+ ├─ pdfs/    
+ ├─ fonts/DejaVuSans.ttf# 生成的 PDF 文件
  └─ templates/
      ├─ index.html          # 浏览器上传页面
      └─ admin.html          # 管理端页面
