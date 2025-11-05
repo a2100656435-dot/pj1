0c77604655310b44e4c68663b2921c5ac4ad2efb
a2100656435-dot <a2100656435@gmail.com>
2025-11-05 15:12:04 -0500

    Update runoob-test.sh

--- runoob-test.sh
+++ runoob-test.sh
@@ -1,11 +1,9 @@
 project/
  ├─ app.py
  ├─ requirements.txt
- ├─ runtime.txt
  ├─ uploads/
  ├─ pdfs/
- ├─ fonts/
- │    └─ DejaVuSans.ttf
+ ├─ fonts/DejaVuSans.ttf
  └─ templates/
       ├─ index.html
       └─ admin.html
