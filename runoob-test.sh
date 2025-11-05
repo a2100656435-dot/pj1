623f4f1b51945343e871527a5bc065e3c077295d
a2100656435-dot <a2100656435@gmail.com>
2025-11-05 15:18:43 -0500

    Update runoob-test.sh

--- runoob-test.sh
+++ runoob-test.sh
@@ -1,9 +1,10 @@
 project/
  ├─ app.py
  ├─ requirements.txt
- ├─ uploads/
- ├─ pdfs/
+ ├─ uploads/           # 临时上传文件
+ ├─ pdfs/              # 生成的 PDF
  ├─ fonts/DejaVuSans.ttf
  └─ templates/
       ├─ index.html
       └─ admin.html
+ └─ Dockerfile
